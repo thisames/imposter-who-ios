ccc57d0a169f651ae3973e4fc7dbbd18a8dd92ad
thisames <thiiagosantos300@gmail.com>
2025-12-25 23:28:22 -0300

    Initial Commit

---
+++ imposter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  imposter
+//
+//  Created by Thiago  dos Santos Gomes on 25/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ imposter/imposterApp.swift
@@ -0,0 +1,17 @@
+//
+//  imposterApp.swift
+//  imposter
+//
+//  Created by Thiago  dos Santos Gomes on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct imposterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
